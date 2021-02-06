56233162035c01bb3cd5d84118e34e2b8cc2f09e
Ivan Tee <e0319256@u.nus.edu>
2021-02-07 02:22:17 +0800

    Supports humidity resolvers

--- src/app/services/resolvers/humidity-resolver.service.ts
+++ src/app/services/resolvers/humidity-resolver.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpRetrieveService } from './http-retrieve.service';
+import { HttpRetrieveService } from '../http-retrieve.service';
 import { Resolve } from '@angular/router';
-import { httpResponse } from '../shared/httpResponse';
+import { httpResponse } from '../../shared/httpResponse';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RainfallResolverService implements Resolve<httpResponse>{
+export class HumidityResolverService implements Resolve<httpResponse>{
 
   constructor(private service : HttpRetrieveService) { }
 
   resolve() : Observable<any> | Promise<any> | any {
-    var output = this.service.getHttpResponse("rainfall", "2020-01-30");
-    console.log("Resolver: ");
+    var output = this.service.getHttpResponse("relative-humidity", "2020-01-30");
+    console.log("Humidity Resolver: ");
     console.log(output.toPromise());
     return output;
   }
--- src/app/services/resolvers/rainfall-resolver.service.ts
+++ src/app/services/resolvers/rainfall-resolver.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpRetrieveService } from './http-retrieve.service';
+import { HttpRetrieveService } from '../http-retrieve.service';
 import { Resolve } from '@angular/router';
-import { httpResponse } from '../shared/httpResponse';
+import { httpResponse } from '../../shared/httpResponse';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TempResolverService implements Resolve<httpResponse> {
+export class RainfallResolverService implements Resolve<httpResponse>{
 
   constructor(private service : HttpRetrieveService) { }
 
   resolve() : Observable<any> | Promise<any> | any {
-    var output = this.service.getHttpResponse("air-temperature", "2020-01-30");
+    var output = this.service.getHttpResponse("rainfall", "2020-01-30");
     console.log("Resolver: ");
     console.log(output.toPromise());
     return output;
